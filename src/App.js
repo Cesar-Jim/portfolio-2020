3e6018bd511021d9a68c67ec2fad4d05321925d8
Cesar Jimenez <cijgr@hotmail.com>
2020-01-27 22:06:20 -0600

    basic navbar finished

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { Route } from 'react-router-dom';
 
-import Header from './components/top/Header';
-import NavBar from './components/top/NavBar';
+import Header from './components/header/Header';
+import NavBar from './components/header/NavBar';
 
 const App = () => {
   return (
--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -3,18 +3,19 @@ import React from 'react';
 const NavBar = () => (
   <nav className='navbar'>
     <div className='navbar__logo-box'>
-      <h1 className='navbar__logo'>
-        <span className='navbar__heading-primary--name'>Cesar</span>
-        <span className='navbar__heading-primary--last'>Jimenez</span>
-        <span className='navbar__heading-primary--last'>Web Developer</span>
-      </h1>
+      <img
+        className='navbar__logo'
+        src='/images/logo-placeholder.png'
+        alt='Logo'
+      />
     </div>
     <div className='navbar__list-box'>
       <ul className='navbar__list'>
-        <li className='navbar__item'>Skills</li>
-        <li className='navbar__item'>Work</li>
-        <li className='navbar__item'>LinkedIn</li>
-        <li className='navbar__item'>GitHub</li>
+        <li className='navbar__item'>Item1</li>
+        <li className='navbar__item'>Item2</li>
+        <li className='navbar__item'>Item3</li>
+        <li className='navbar__item'>Item4</li>
+        <li className='navbar__item'>Item5</li>
       </ul>
     </div>
   </nav>
