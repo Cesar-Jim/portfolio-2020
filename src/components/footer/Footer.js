09ca8e23f3e85adabc9fd049f7d0f162ca9a2afe
Cesar Jiménez <cijgr@hotmail.com>
2020-05-05 09:28:52 -0500

    added positive ssl logo and tweaked the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className='footer__container'>
-      <div className='footer__flag'>
-        <div className='footer__flag--proudly'>Proudly</div>
-        <div className='footer__flag--made-in'>&nbsp;Made In&nbsp;</div>
-        <div className='footer__flag--mexico'>Mexico.</div>
+    <div className="footer__container">
+      <div className="footer__flag">
+        <div className="footer__flag--proudly">Proudly</div>
+        <div className="footer__flag--made-in">&nbsp;Made In&nbsp;</div>
+        <div className="footer__flag--mexico">Mexico.</div>
       </div>
-      <div className='footer__copyright'>
+      <div className="footer__copyright">
         <p>Copyright © 2020 Cesar Jimenez | Mexico | All rights reserved.</p>
+      </div>
+      <div className="footer__positiveSSL">
+        <img
+          className="footer__positiveSSL--logo"
+          src="/images/positive-ssl.png"
+          alt="Positive SSL Logo"
+        />
       </div>
     </div>
   );
